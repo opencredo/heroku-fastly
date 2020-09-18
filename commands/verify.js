6d6a2e2a96c9195cde8611c8a6a188fcf2f81ad9
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-09-18 15:56:51 +0100

    Updates to handle a status of renewing

--- commands/verify.js
+++ commands/verify.js
@@ -95,8 +95,8 @@ function processVerifyResponse(data, domain) {
 
   hk.styledHeader(`Domain ${domain} TLS subscription state: ${status}`);
 
-  if(status === 'issued'){
-    hk.log(`Domain ${domain} now supports TLS.`);
+  if(status === 'issued' || status === 'renewing'){
+    hk.log(`Domain ${domain} supporting TLS.`);
   } else {
     hk.log(`The issuing of a certificate may take up to 30 minutes.  In the mean time please confirm your DNS records are configured with your DNS provider for ${domain}`);
   }
